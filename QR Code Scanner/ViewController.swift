be532f257d092cde6f41934410cca4db1718f69d
Vijay <vijaysn02@gmail.com>
2020-04-29 11:22:54 +0530

    Initial Commit

---
+++ QR Code Scanner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QR Code Scanner
+//
+//  Created by TPFLAP146 on 29/04/20.
+//  Copyright © 2020 vijay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
